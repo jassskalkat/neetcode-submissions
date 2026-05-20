560b28587513c1cfebbb9d3c7af3df2943486d32
Japneet Kalkat <59381133+jassskalkat@users.noreply.github.com>
2026-05-20 12:50:21 -0300

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,12 @@
+integer_type = "<class 'int'>"
+float_type = "<class 'float'>"
+bool_type = "<class 'bool'>"
+string_type = "<class 'str'>"
+list_type = "<class 'list'>"
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
