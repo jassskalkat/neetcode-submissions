73fd06ad8acb9386a0bb5f4991319ee277481012
Japneet Kalkat <59381133+jassskalkat@users.noreply.github.com>
2026-05-20 14:13:13 -0300

    Add: python-control-flow - submission-0

---
+++ Python For Beginners/python-control-flow/submission-0.py
@@ -0,0 +1,16 @@
+for i in range(1, 8):
+    pass
+
+if True:
+    pass
+
+def unfinsished_function():
+    pass
+
+for i in range(1, 8):
+    pass
+
+for i in range(1, 8):
+    pass
+
+print("nothing else happened")
