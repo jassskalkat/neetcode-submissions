a320298b3e9b9919639f3dc13c6b430d66b03843
Japneet Kalkat <59381133+jassskalkat@users.noreply.github.com>
2026-05-20 13:57:37 -0300

    Add: python-logic-condition - submission-0

---
+++ Python For Beginners/python-logic-condition/submission-0.py
@@ -0,0 +1,10 @@
+def discount_applies(age: int) -> bool:
+  return 65 <= age < 18
+
+
+# don't modify this code below this line
+print(discount_applies(17))
+print(discount_applies(18))
+print(discount_applies(40))
+print(discount_applies(65))
+print(discount_applies(70))
